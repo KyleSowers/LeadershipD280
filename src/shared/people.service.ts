013ae08ea17b95f21bb1b7031ca19eb46717f70a
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-10-18 15:47:56 -0400

    Creates a dataset for the export of the PeopleService.

--- src/shared/people.service.ts
+++ src/shared/people.service.ts
@@ -7,4 +7,29 @@ import { Observable, of } from 'rxjs';
 export class PeopleService {
 
   constructor() { }
+
+  getPeople(): Observable<any> {
+    let peopleArray = [{
+      firstName: 'James',
+      lastName: 'Dimon',
+      jobTitle: 'Chairman of the Board and Chief Executive Officer'
+    }, {
+      firstName: 'Lori',
+      lastName: 'Beer',
+      jobTitle: 'Global Chief Information Officer'
+    }, {
+      firstName: 'Thasunda',
+      lastName: 'Duckett',
+      jobTitle: 'CEO, Consumer Banking'
+    }, {
+      firstName: 'Mary',
+      lastName: 'Erdoes',
+      jobTitle: 'Asset & Wealth Management CEO'
+    }, {
+      firstName: 'Carlos',
+      lastName: 'Hernandez',
+      jobTitle: 'Executive Chair of Investment & Corporate Banking'
+    }];
+    return of(peopleArray);
+  }
 }
